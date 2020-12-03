deb262d20a33f6245492c2ed4d0c2b7269480ed3
Vladislav Churyla <tagirovicv@gmail.com>
2020-12-03 23:37:45 +0300

    Main startup class

---
+++ src/main/java/by/bsuir/blurbot/Main.java
@@ -0,0 +1,9 @@
+package by.bsuir.blurbot;
+
+import by.bsuir.blurbot.context.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.start();
+    }
+}
